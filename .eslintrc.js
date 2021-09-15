2b18e253c9a2b997afab6fed5240fef0e9d32b15
Sviatozar <svpetrenko123@gmail.com>
2021-09-15 23:14:51 +0300

    add other utilities

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["google"],
+    extends: ["metarhia"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
